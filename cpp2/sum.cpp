96d712e085c752ab91f6b1f5e2ecd73a5fe53301
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-12-17 22:44:05 +0100

    Add cpp2

---
+++ cpp2/sum.cpp
@@ -0,0 +1,15 @@
+// Include basic libraries
+#include <iostream>
+
+// Tell the environment to use standard namespace, this avoid having to write "std::" before every expression
+using namespace std;
+
+int main() {
+	// Define two numbers
+	int a = 1, b = 2;
+	// Sum the numbers
+	int sum = a + b;
+
+	// Print the result
+	cout << "Result: " << sum << ".\n";
+}
