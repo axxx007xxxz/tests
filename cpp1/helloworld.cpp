3bae0c7d257082a9156b0f9c771b3336cab5e405
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-12-17 22:32:33 +0100

    Add cpp1
    "Because everything starts with an hello world!" :p

---
+++ cpp1/helloworld.cpp
@@ -0,0 +1,9 @@
+// Include basic libraries
+#include <iostream>
+
+// Tell the environment to use standard namespace, this avoid having to write "std::" before every expression
+using namespace std;
+
+int main() {
+	cout << "Hello world!\n";
+}
